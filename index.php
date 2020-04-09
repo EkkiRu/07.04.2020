7a9f5ee95e285033412fe24db961fc87b06a1b14
EkkiRu <63056889+EkkiRu@users.noreply.github.com>
2020-04-09 13:47:21 +0600

    Ekki
    2 лаба

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'template/header.php';
+require_once 'template/footer.php';
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
